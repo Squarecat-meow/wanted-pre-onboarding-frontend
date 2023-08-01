8ed325664162af23c880bb46b36dcc01ac0e08f6
Squarecat-meow <kw7551@gmail.com>
2023-08-01 09:59:37 +0900

    implement regex for login & signup

--- src/components/SigninComp.jsx
+++ src/components/SigninComp.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SigninComp = () => {
+  const [emailChecked, setEmailChecked] = useState(false);
+  const [pwChecked, setPwChecked] = useState(false);
+
+  const emailCheck = (e) => {
+    const emailRegEx = /[@]/;
+
+    const isEmailCorrect = emailRegEx.test(e);
+    if (isEmailCorrect === true) {
+      setEmailChecked(true);
+    } else {
+      setEmailChecked(false);
+    }
+  };
+
+  const passwordCheck = (e) => {
+    const passwordRegEx = /.{8,}/;
+
+    const isPasswordCorrect = passwordRegEx.test(e);
+    if (isPasswordCorrect === true) {
+      setPwChecked(true);
+    } else {
+      setPwChecked(false);
+    }
+  };
   return (
     <div>
-      <input data-testid="email-input" placeholder="email" />
-      <input data-testid="password-input" placeholder="password" />
+      <input
+        data-testid="email-input"
+        placeholder="email"
+        onChange={(e) => emailCheck(e.target.value)}
+        type="email"
+      />
+      <input
+        data-testid="password-input"
+        placeholder="password"
+        onChange={(e) => passwordCheck(e.target.value)}
+        type="password"
+      />
+      <button
+        data-testid="signin-button"
+        disabled={!(emailChecked && pwChecked)}
+      >
+        로그인
+      </button>
     </div>
   );
 };
--- src/components/SignupComp.jsx
+++ src/components/SignupComp.jsx
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const SignupComp = () => {
-  return <div>SignupComp</div>;
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+
+  const [emailChecked, setEmailChecked] = useState(false);
+  const [pwChecked, setPwChecked] = useState(false);
+
+  const [loginForm, setLoginForm] = useState({
+    emailAddr: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    emailCheck(email);
+    passwordCheck(pw);
+  }, [email, pw, loginForm]);
+
+  const emailCheck = (e) => {
+    const emailRegEx = /[@]/;
+
+    const isEmailCorrect = emailRegEx.test(e);
+    if (isEmailCorrect === true) {
+      setEmailChecked(true);
+    } else {
+      setEmailChecked(false);
+    }
+  };
+
+  const passwordCheck = (e) => {
+    const passwordRegEx = /.{8,}/;
+
+    const isPasswordCorrect = passwordRegEx.test(e);
+    if (isPasswordCorrect === true) {
+      setPwChecked(true);
+    } else {
+      setPwChecked(false);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setLoginForm({
+      emailAddr: email,
+      password: pw,
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          data-testid="email-input"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          name="email"
+        />
+        <input
+          data-testid="password-input"
+          placeholder="password"
+          onChange={(e) => setPw(e.target.value)}
+          type="password"
+          name="password"
+        />
+        <button
+          data-testid="signup-button"
+          disabled={!(emailChecked && pwChecked)}
+          type="submit"
+        >
+          회원가입
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignupComp;
